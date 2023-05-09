b8f0f5d0950fb6fad1e6dcab80af5c4769d6f32e
thiennn262626 <nguyendinhthien10a5@gmail.com>
2023-05-09 16:20:28 +0700

    style of style

--- app/src/main/java/vn/iotstar/cosmeticshopapp/api/APIService.java
+++ app/src/main/java/vn/iotstar/cosmeticshopapp/api/APIService.java
@@ -20,6 +20,8 @@ public interface APIService {
 
     @GET("category")
     Call<CategoryAndStyleResponse> getCategory();
+    @GET("style")
+    Call<CategoryAndStyleResponse> getStyle();
 
     @POST("product/getRandomProduct")
     @FormUrlEncoded
