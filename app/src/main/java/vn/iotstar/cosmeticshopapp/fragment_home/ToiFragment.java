043d6fe29957be91e8e333253333399c03169a3b
thiennn262626 <nguyendinhthien10a5@gmail.com>
2023-05-11 17:36:09 +0700

    toi

--- app/src/main/java/vn/iotstar/cosmeticshopapp/fragment_home/ToiFragment.java
+++ app/src/main/java/vn/iotstar/cosmeticshopapp/fragment_home/ToiFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,45 +12,35 @@ import androidx.fragment.app.Fragment;
 
 import vn.iotstar.cosmeticshopapp.LoginSignupActivity;
 import vn.iotstar.cosmeticshopapp.R;
+import vn.iotstar.cosmeticshopapp.sharedPref.SharedPrefManager;
+
 
 public class ToiFragment extends Fragment {
     View view;
-    TextView txtTenUser;
-    LinearLayout lnChuaLogin, lnDaLogin;
+    TextView txtLogin;
+    SharedPrefManager sharedPrefManager;;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_toi, container, false);
+
         anhXa();
-        setOneClick();
-        checkLogin();
+        if (sharedPrefManager.getUser().getId() == -1){
+            txtLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "1", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getContext(), LoginSignupActivity.class);
+                    startActivity(intent);
+                }
+            });
+        } else {
+            txtLogin.setText("Xin chào " + sharedPrefManager.getUser().getFirstName() + " " + sharedPrefManager.getUser().getLastName());
+        }
         return view;
     }
-
-    private void checkLogin() {
-        if (true){
-            lnChuaLogin.setVisibility(View.GONE);
-            lnDaLogin.setVisibility(View.VISIBLE);
-            txtTenUser.setText("Nguyễn Văn A");
-        }else {
-            lnChuaLogin.setVisibility(View.VISIBLE);
-            lnDaLogin.setVisibility(View.GONE);
-        }
-    }
-
-    private void setOneClick() {
-        lnChuaLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getContext(), LoginSignupActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
     private void anhXa(){
-        lnChuaLogin = view.findViewById(R.id.lnChualogin);
-        lnDaLogin = view.findViewById(R.id.lnDalogin);
-        txtTenUser = view.findViewById(R.id.txtTenUser);
+        txtLogin = view.findViewById(R.id.txtDangNhap);
+        sharedPrefManager = new SharedPrefManager(getContext());
     }
 }
