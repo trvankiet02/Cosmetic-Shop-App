d83f9102546adaf6d9cfcbccf38cc909160d4d3b
thiennn262626 <nguyendinhthien10a5@gmail.com>
2023-05-12 15:34:09 +0700

    12 05

--- app/src/main/java/vn/iotstar/cosmeticshopapp/fragment_home/ToiFragment.java
+++ app/src/main/java/vn/iotstar/cosmeticshopapp/fragment_home/ToiFragment.java
@@ -12,10 +12,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import vn.iotstar.cosmeticshopapp.LoginSignupActivity;
 import vn.iotstar.cosmeticshopapp.R;
 import vn.iotstar.cosmeticshopapp.SettingActivity;
+import vn.iotstar.cosmeticshopapp.XuLyDonHangActivity;
+import vn.iotstar.cosmeticshopapp.fragment_xylydonhang.ChoXacNhanFragment;
+import vn.iotstar.cosmeticshopapp.fragment_xylydonhang.DaHuyFragment;
+import vn.iotstar.cosmeticshopapp.fragment_xylydonhang.DaNhanFragment;
+import vn.iotstar.cosmeticshopapp.fragment_xylydonhang.DaXacNhanFragment;
+import vn.iotstar.cosmeticshopapp.fragment_xylydonhang.DangGiaoFragment;
 import vn.iotstar.cosmeticshopapp.sharedPreferentManager.SharedPrefManager;
 
 
@@ -24,6 +31,7 @@ public class ToiFragment extends Fragment {
     ImageView btnProfile;
     TextView txtLogin;
     SharedPrefManager sharedPrefManager;
+    LinearLayout lnChuaXacNhan, lnDaXacNhan, lnDangVanChuyen, lnDaNhan, lnDaHuy;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class ToiFragment extends Fragment {
 
         anhXa();
         setBtnProfile();
+        setBtnXyLyDonHang();
         if (sharedPrefManager.getUser().getId() == -1){
             txtLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -45,10 +54,26 @@ public class ToiFragment extends Fragment {
         }
         return view;
     }
+
+    private void setBtnXyLyDonHang() {
+        lnChuaXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), XuLyDonHangActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
     private void anhXa(){
         txtLogin = view.findViewById(R.id.txtDangNhap);
         sharedPrefManager = new SharedPrefManager(getContext());
         btnProfile = view.findViewById(R.id.btnProfile);
+        lnChuaXacNhan = view.findViewById(R.id.lnChuaXacNhan);
+        lnDaXacNhan = view.findViewById(R.id.lnDaXacNhan);
+        lnDangVanChuyen = view.findViewById(R.id.lnDangVanChuyen);
+        lnDaNhan = view.findViewById(R.id.lnDaNhan);
+        lnDaHuy = view.findViewById(R.id.lnDaHuy);
     }
     private void setBtnProfile(){
         btnProfile.setOnClickListener(new View.OnClickListener() {
